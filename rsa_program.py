ec5fe9b309b5972c205d3d175afdc06b06703d96
Guinueng <admin@guinueng.com>
2025-03-25 22:37:12 +0900

    Modified structure a little bit to gather it.

--- rsa_program.py
+++ rsa_program.py
@@ -1,14 +1,17 @@
 import sys
 
-def gcd(a, b): # Euclidean Algorithm to get gcd.
-   if(b == 0): # If base case(b = 0) occurs, return a which is gcd of first input.
+# Euclidean Algorithm to get gcd.
+def gcd(a, b):
+   # If base case(b = 0) occurs, return a which is gcd of first input.
+   if(b == 0):
       return a
    else: # Else, pursue Euclidean Algorithm.
       q = a % b
       rst = gcd(b, q)
       return rst
 
-def rsa(msg, key, n):   # RSA function. Returns m^key mod n.
+# RSA function. Returns m^key mod n.
+def rsa(msg, key, n):
    return (msg ** key) % n
 
 def rsa_encrypt(p_txt, e, n):
@@ -35,20 +38,13 @@ def rsa_decrypt(c_txt, d, n):
 
    return p_txt
 
-print(sys.argv)
-print(sys.argv[1])
-
 if(sys.argv[1] == "--generate-key"):
    # Get required file name on argv.
    # Reference: https://wikidocs.net/26
-   if sys.argv[2] == "--p":
-      p = int(sys.argv[3])
-      q = int(sys.argv[5])
-   
-   if sys.argv[2] == "--q":
-      q = int(sys.argv[3])
-      p = int(sys.argv[5])
- 
+
+   p = int(sys.argv[3])
+   q = int(sys.argv[5])
+
    # Calculate n, phi.
    n = p * q
    phi = (p - 1) * (q - 1)
@@ -70,9 +66,8 @@ if(sys.argv[1] == "--generate-key"):
                d += 1
 
             # while(b > 0): # Extended Euclidean Algorithm to get e, d values.
-            # During usage of extended euclidean algorithm, example case does not worked.
-            # Due to this problem, I decided to use brute-force algorithm.
-            #    # print(b)
+            # # During usage of extended euclidean algorithm, example case does not worked.
+            # # Due to this problem, I decided to use brute-force algorithm.
             #    q = a // b # quotient
             #    r = a % b # remainder
             #    a = b # update value.
@@ -102,20 +97,17 @@ if(sys.argv[1] == "--generate-key"):
    print("d={0}".format(d))
    print("phi={0}".format(phi))
 
-   pub = open("public_key.txt", "w")   # Print result into screen and file.
-   pri = open("private_key.txt", "w")
-
    # Concat n and e into two line and write it into public_key.txt file.
-   pub_txt = "n=" + str(n) + "\ne=" + str(e)
-   pub.write(pub_txt)
+   pub_str = "n=" + str(n) + "\ne=" + str(e)
+   pub_txt_file = open("public_key.txt", "w")
+   pub_txt_file.write(pub_str)
+   pub_txt_file.close()
 
    # Concat n and d into two line and write it into private_key.txt file.
-   pri_txt = "n=" + str(n) + "\nd=" + str(d)   
-   pri.write(pri_txt)
-
-   # Close public_key.txt and private_key.txt file.
-   pub.close()
-   pri.close()
+   pri_str = "n=" + str(n) + "\nd=" + str(d)
+   pri_txt_file = open("private_key.txt", "w")
+   pri_txt_file.write(pri_str)
+   pri_txt_file.close()
 
 if(sys.argv[1] == "--encrypt"):
    # Get required file name on argv.
@@ -123,20 +115,20 @@ if(sys.argv[1] == "--encrypt"):
    pub_key_name = sys.argv[4]
    c_txt_name = sys.argv[6]
 
-   # Open required files.
+   # Open, read and close plain text and public key file.
    p_txt_file = open(p_txt_name, "r")
-   pub_key_file = open(pub_key_name, "r")
-   c_txt_file = open(c_txt_name, "w")
-
-   # Read plain text and public_key file.
    p_txt = p_txt_file.read()
+   p_txt_file.close()
+
+   pub_key_file = open(pub_key_name, "r")
    pub_key_txt = pub_key_file.read()
+   pub_key_file.close()
 
    c_txt = ""
    n = ""
    e = ""
 
-   # Parsing n and d into given public_key.txt file.
+   # Parsing n and e into given public_key.txt file.
    e_line = False
    for i in pub_key_txt:
       if(i == "\n"):
@@ -151,17 +143,13 @@ if(sys.argv[1] == "--encrypt"):
             e += i
 
    # Encrypt plain text into cipher text by using rsa method.
-   c_txt = rsa_encrypt(p_txt, int(e), int(n))
-
    # Delete unnecessary space in last of cipher text.
-   c_txt = c_txt[:-1]
+   c_txt = rsa_encrypt(p_txt, int(e), int(n))[:-1]
 
-    # Print calculated cipher text result and write it into ciphertext.txt file.
-   print("Ciphertext:", c_txt) 
+   # Print converted cipher text result and write it into ciphertext.txt file.
+   print("Ciphertext:", c_txt)
+   c_txt_file = open(c_txt_name, "w")
    c_txt_file.write(c_txt)
-
-   p_txt_file.close()
-   pub_key_file.close()
    c_txt_file.close()
 
 if(sys.argv[1] == "--decrypt"):
@@ -170,15 +158,16 @@ if(sys.argv[1] == "--decrypt"):
    pri_key_name = sys.argv[4]
    p_txt_name = sys.argv[6]
 
-   # Open required files.
+   # Open, read, and close cipher text and private key files.
    c_txt_file = open(c_txt_name, "r")
-   pri_key_file = open(pri_key_name, "r")
-   p_txt_file = open(p_txt_name, "w")
-
-   # Read plain text and private_key file.
    c_txt = c_txt_file.read()
-   pri_key_txt = pri_key_file.read()
+   c_txt_file.close()
 
+   pri_key_file = open(pri_key_name, "r")
+   pri_key_txt = pri_key_file.read()
+   pri_key_file.close()
+
+   
    # Add space on last part of cipher text to be sure calculate last hex number into text.
    c_txt += " "
    p_txt = ""
@@ -202,13 +191,10 @@ if(sys.argv[1] == "--decrypt"):
    # Decrypt cipher text into plain text by using rsa method.
    p_txt = rsa_decrypt(c_txt, int(d), int(n))
 
-   # Print calculated plain text and save it into plaintext.txt file.
-   print("Decrypted plaintext:",p_txt)
+   # Print converted plain text and save it into plaintext.txt file.
+   print("Decrypted plaintext:", p_txt)
+   p_txt_file = open(p_txt_name, "w")
    p_txt_file.write(p_txt)
-
-   # Close public_key.txt and private_key.txt file.
-   c_txt_file.close()
-   pri_key_file.close()
    p_txt_file.close()
 
 if(sys.argv[1] == "--sign"):
@@ -217,17 +203,15 @@ if(sys.argv[1] == "--sign"):
    pri_key_name = sys.argv[4]
    sign_txt_name = sys.argv[6]
 
-   # Open required files.
+   # Open, read, and close required files.
    pri_key_file = open(pri_key_name, "r")
-   sign_txt_file = open(sign_txt_name, "w")
-
-   # Read private_key file.
    pri_key_txt = pri_key_file.read()
+   pri_key_file.close()
 
    n = ""
    d = ""
 
-   # Parsing n and e into given public_key.txt file.
+   # Parsing n and d into given private_key.txt file.
    d_line = False
    for i in pri_key_txt:
       if(i == "\n"):
@@ -241,32 +225,37 @@ if(sys.argv[1] == "--sign"):
          if(i != "d" and i != "="):
             d += i
 
+   # Encrypt signature text into cipher text by using rsa method.
    digi_sign = rsa_encrypt(sign, int(d), int(n))
+
+   # Delete unnecessary space in last of signature text.
    digi_sign = digi_sign[:-1]
 
+   # Print created digital signature result, open signature.txt,
+   # write result into file, and close it.
    print("Signature:", digi_sign)
+   sign_txt_file = open(sign_txt_name, "w")
    sign_txt_file.write(digi_sign)
-
-   # Close private_key.txt and signature.txt file.
-   pri_key_file.close()
    sign_txt_file.close()
 
 if(sys.argv[1] == "--verify"):
    print("-–verify case")
-   # Get required file name on argv.
+   # Get required file name and string on argv.
    verify_str = sys.argv[2]
    sign_txt_name = sys.argv[4]
    pub_key_name = sys.argv[6]
 
-   # Open required files.
+   # Open, read and close signature file.
    sign_txt_file = open(sign_txt_name, "r")
-   pub_key_file = open(pub_key_name, "r")
-
-   # Read plain text and private_key file.
    sign_txt = sign_txt_file.read()
-   pub_key_txt = pub_key_file.read()
+   sign_txt_file.close()
 
-   # Add space on last part of cipher text to be sure calculate last hex number into text.
+   # Open, read and close public_key file.
+   pub_key_file = open(pub_key_name, "r")
+   pub_key_txt = pub_key_file.read()
+   pub_key_file.close()
+
+   # Add space on last part of signature text to be surely converted last hex number into text.
    sign_txt += " "
    n = ""
    e = ""
@@ -287,15 +276,7 @@ if(sys.argv[1] == "--verify"):
 
    compare_str = rsa_decrypt(sign_txt, int(e), int(n))
 
-   print(compare_str)
-
    if(verify_str == compare_str):
       print("Signature is valid")
    else:
       print("Signature is invalid")
-
-   # Close signature.txt and public_key.txt file.
-   sign_txt_file.close()
-   pub_key_file.close()
-
-# –
