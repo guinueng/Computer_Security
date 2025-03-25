b800c5dc53cf9c56e9bd33be8bee4a00bc801ca0
Guinueng <admin@guinueng.com>
2025-03-25 21:27:28 +0900

    Implemented Decryption function.

--- rsa_program.py
+++ rsa_program.py
@@ -15,7 +15,6 @@ print(sys.argv)
 print(sys.argv[1])
 
 if(sys.argv[1] == "--generate-key"):
-   print("-–generate-key case")
    if sys.argv[2] == "--p":   # Get required file name on argv.
       # Reference: https://wikidocs.net/26
       p = int(sys.argv[3])
@@ -66,84 +65,32 @@ if(sys.argv[1] == "--generate-key"):
                break
 
    print("RSA key pair generated:")
-   print("n=", n)
-   print("e=", e)
-   print("d=", d)
-   print("phi=",phi)
+   print("n={0}".format(n))         # By using {0} and .format, we can print decimal and string together.
+   print("e={0}".format(e))         # Reference: https://wikidocs.net/164969
+   print("d={0}".format(d))
+   print("phi={0}".format(phi))
 
    pub = open("public_key.txt", "w")   # Print result into screen and file.
    pri = open("private_key.txt", "w")
 
-   pub_txt = "n=" + str(n) + "\ne=" + str(e)
-   pri_txt = "n=" + str(n) + "\nd=" + str(d)
-   pub.write(pub_txt)
-   pri.write(pri_txt)
+   pub_txt = "n=" + str(n) + "\ne=" + str(e) # Concat n and e into two line.
+   pri_txt = "n=" + str(n) + "\nd=" + str(d) # Concat n and d into two line.
+   pub.write(pub_txt)   # Write public_key line into public_key.txt
+   pri.write(pri_txt)   # Write public_key line into private_key.txt
 
-   pub.close()
+   pub.close() # Close public_key.txt and private_key.txt file.
    pri.close()
 
 if(sys.argv[1] == "--encrypt"):
-   print("--encrypt case")
    p_txt_name = sys.argv[2]   # Get required file name on argv.
-   pri_key_name = sys.argv[4]
+   pub_key_name = sys.argv[4]
    c_txt_name = sys.argv[6]
-
-   c_txt_file = open(p_txt_name, "r")     # Open required files.
-   pri_key_file = open(pri_key_name, "r")
-   p_txt_file = open(c_txt_name, "w")
-
-   c_txt = c_txt_file.read()        # Read plain text and public_key fille
-   pri_key_txt = pri_key_file.read()
-
-   p_txt = ""
-   n = ""
-   d = ""
-
-   d_line = False
-   for i in pri_key_txt: # Parsing n and d into given private_key.txt file.
-      if(i == "\n"):
-         d_line = True
-         continue
-
-      if(not(d_line)):
-         if(i != "n" and i != "="):
-            n += i
-      else:
-         if(i != "e" and i != "="):
-            d += i
-
-   n = int(n)  # Conv parsed n and e str to int.
-   d = int(d)
-
-   for i in c_txt:
-      t_str = ord(i)   # ord function converts input character into corresponding ascii.
-      # Reference: https://www.quora.com/How-do-you-convert-ascii-to-integer-in-Python
-      p_txt += str(ascii(rsa(t_str, d, n)))
-
-   p_txt = p_txt[:-1]
-   print(p_txt)
-   print(len(p_txt))
-   # Hello, RSA!
-
-   p_txt_file.write(p_txt)
-
-   p_txt_file.close()
-   pri_key_file.close()
-   c_txt_file.close()
-
-if(sys.argv[1] == "-–decrypt"):
-   print("-–decrypt case")
-   c_txt_name = sys.argv[2]   # Get required file name on argv.
-   pri_key_name = sys.argv[4]
-   p_txt_name = sys.argv[6]
-
-   print(c_txt_name, pub_key_name, p_txt_name)
 
    p_txt_file = open(p_txt_name, "r")     # Open required files.
    pub_key_file = open(pub_key_name, "r")
    c_txt_file = open(c_txt_name, "w")
 
-   p_txt = p_txt_file.read()
+   p_txt = p_txt_file.read()        # Read plain text and public_key fille
    pub_key_txt = pub_key_file.read()
 
    c_txt = ""
@@ -151,7 +98,7 @@ if(sys.argv[1] == "-–decrypt"):
    e = ""
 
    e_line = False
-   for i in pub_key_txt: # Parsing n and e into given public_key.txt file.
+   for i in pub_key_txt: # Parsing n and d into given public_key.txt file.
       if(i == "\n"):
          e_line = True
          continue
@@ -168,18 +115,79 @@ if(sys.argv[1] == "-–decrypt"):
 
    for i in p_txt:
       t_str = ord(i)   # ord function converts input character into corresponding ascii.
-      c_txt += (hex(rsa(t_str, e, n)) + " ")
+      # Reference: https://www.quora.com/How-do-you-convert-ascii-to-integer-in-Python
+      c_txt += (str(hex(rsa(t_str, e, n))) + " ")
 
-   c_txt = c_txt[:-1]
-   print(c_txt)
-   print(len(c_txt))
-   # 0x43f 0xbff 0x755 0x755 0xc6f 0x469 0xad6 0x435 0x721 0x525 0x971
-
-   c_txt_file.write(c_txt)
+   c_txt = c_txt[:-1]      # Delete unnecessary space in last of cipher text.
+   print("Ciphertext:", c_txt)  # Print calculated cipher text result.
+   c_txt_file.write(c_txt) # Write calculated cipher text into ciphertext.txt file.
 
    p_txt_file.close()
    pub_key_file.close()
    c_txt_file.close()
+
+if(sys.argv[1] == "--decrypt"):
+   print("-–decrypt case")
+   c_txt_name = sys.argv[2]   # Get required file name on argv.
+   pri_key_name = sys.argv[4]
+   p_txt_name = sys.argv[6]
+
+   print(c_txt_name, pri_key_name, p_txt_name)
+
+   c_txt_file = open(c_txt_name, "r")     # Open required files.
+   pri_key_file = open(pri_key_name, "r")
+   p_txt_file = open(p_txt_name, "w")
+
+   c_txt = c_txt_file.read()        # Read plain text and public_key fille
+   pri_key_txt = pri_key_file.read()
+
+   c_txt += " "
+   p_txt = ""
+   n = ""
+   d = ""
+
+   d_line = False
+   for i in pri_key_txt: # Parsing n and e into given public_key.txt file.
+      if(i == "\n"):
+         d_line = True
+         continue
+
+      if(not(d_line)):
+         if(i != "n" and i != "="):
+            n += i
+      else:
+         if(i != "d" and i != "="):
+            d += i
+
+   n = int(n)  # Conv parsed n and e str to int.
+   d = int(d)
+
+   print(c_txt)
+
+   tmp_str = ""
+   tmp_num = 0
+   for i in c_txt:
+      if(i == " "):
+         print(tmp_str)
+         t_str = int(tmp_str, 16)
+         print("conv hex:",hex(t_str))
+         print(t_str)
+         p_txt += str(chr(rsa(t_str, d, n)))
+         print("rst:",str(chr(rsa(t_str, d, n))))
+         tmp_str = ""
+      else:
+         tmp_str += i
+
+   # p_txt = p_txt[:-1]
+   print("Decrypted plaintext:",p_txt)
+   print(len(p_txt))
+   # Hello, RSA!
+
+   p_txt_file.write(p_txt)
+
+   c_txt_file.close()
+   pri_key_file.close()
+   p_txt_file.close()
 
 if(sys.argv[1] == "-–sign"):
    print("-–sign case")
@@ -187,5 +195,4 @@ if(sys.argv[1] == "-–sign"):
 if(sys.argv[1] == "-–verify"):
    print("-–verify case")
 
-if(sys.argv[1] == "a"):
-   print("test")
+# –
